6ffb9ee41a2b9ac6f84f968d513a97e7c1823bad
freddy16velas <freddy16velas@outlook.com>
2020-12-17 16:12:46 -0600

    API RESTFULL

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
 const path = require('path')
 
-//middlware
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+const app = express();
+
 
 // simulatin db
 const users =  [
@@ -25,6 +22,11 @@ const users =  [
 // settings
 app.set('port', process.env.PORT || 3000);
 
+//middlware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 // static files 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -33,11 +35,62 @@ app.get ('/usuarios', (req, res)=> {
     res.json(users)
 });
 
-
-app.post ('/usuarios', (req, res)=> { 
+app.post ('/usuarios', (req, res) => {
+    const { Name } = req.body;
+    const { User } = req.body;
+    const { Email } = req.body;
+    const { LstName } = req.body;
+    const { Rol } = req.body;
+    const { Telefono } = req.body;
+    const { Estatus } = req.body;
+    users.push ({
+        id: users.length+1,
+        Name,
+        User,
+        Email,
+        LstName,
+        Rol,
+        Telefono,
+        Estatus
+    })
     console.log(req.body);
-    res.send('datos recibidos ')
+    res.json('Successfully created');
 });
+
+app.put ('/usuarios/:id', (req,res)=>{
+    console.log (req.params, req.body);
+    const { id } = req.params; 
+    const { Name } = req.body;
+    const { User } = req.body;
+    const { Email } = req.body;
+    const { LstName } = req.body;
+    const { Rol } = req.body;
+    const { Telefono } = req.body;
+    const { Estatus } = req.body;
+
+    users.forEach((user, i) => {
+        if (user.id == id) {
+            user.Name = Name;
+            user.User = User;
+            user.Email = Email;
+            user.LstName= LstName;
+            user.Rol = Rol;
+            user.Telefono = Telefono;
+            user.Estatus = Estatus;
+        }
+    })
+    res.json('Succesfully updated');
+});
+app.delete('/usuarios/:id', (req, res) =>{
+    const { id } = req.params;
+    users.forEach((user, i)=> {
+        if (user.id ==id) {
+            users.splice (i, 1)
+        }
+        res.json ('Succesfully deleted');
+    });
+})
+
 
 // starting the server 
 app.listen(app.get ('port'), () => {
--- src/public/app.js
+++ src/public/app.js
@@ -5,7 +5,7 @@ $(function(){
             url: '/usuarios',
             success: function (users){
                let tbody = $('tbody');
-               tbody.html =('');
+               tbody.html('');
                users.forEach(user => {
                    tbody.append(`
                    <tr>
@@ -13,7 +13,7 @@ $(function(){
                      <td>
                        <input type="text" class="User" value="${user.User}"/>
                        <input type="text" class="Name" value="${user.Name}"/>
-                       <input type="text" class="LstName" value="${user.LastName}"/>
+                       <input type="text" class="LstName" value="${user.LstName}"/>
                        <input type="text" class="Email" value="${user.Email}"/>
                        <input type="text" class="Rol" value="${user.Rol}"/>
                        <input type="text" class="Telefono" value="${user.Telefono}"/>
@@ -29,32 +29,76 @@ $(function(){
             }
         })
     });
-    $('#usersForm').on ('submit', function (e){
+    
+   $('#usersForm').on ('submit', function (e){
       e.preventDefault();
-        let nUser = $('#User');
-        let nName = $('#Name');
-        let nLstName = $('#LstName');
-        let nEmail = $('#Email');
-        let nRol = $('#Rol');
-        let nTelefono = $('#Telefono');
-        let nEstatus = $('Estatus');
+        let User = $('#nUser');
+        let Name = $('#nName');
+        let LstName = $('#nLstName');
+         let Email = $('#nEmail');
+        let Rol = $('#nRol');
+        let Telefono = $('#nTelefono');
+        let Estatus = $('nEstatus');
 
         $.ajax({
             url: '/usuarios',
             method: 'POST',
             data: {
-                Name: nName.val(),
-                User: nUser.val(),
-                LstName: nLstName.val(),
-                Email: nEmail.val(),
-                Rol: nRol.val(),
-                Telefono: nTelefono.val(),
-                Estatus: nEstatus.val()
+                Name: Name.val(),
+                User: User.val(),
+                LstName: LstName.val(),
+                Email: Email.val(),
+                Rol: Rol.val(),
+                Telefono: Telefono.val(),
+                Estatus: Estatus.val()
             },
             success: function(response) {
-                console.log(response)
-                
+               $('#getUsers').click();
             }
       })
+    });
+    $('table').on('click', '.update-button', function(){
+      let row = $(this).closest('tr');
+      let id = row.find('.id').text();
+      let Name = row.find('.Name').val();
+      let User = row.find('.User').val();
+      let LstName = row.find('.LstName').val();
+      let Email = row.find('.Email').val();
+      let Rol = row.find('.Rol').val();
+      let Telefono = row.find('.Telefono').val();
+      let Estatus = row.find('.Estatus').val();
+
+      $.ajax ({
+        url: "/usuarios/"+ id,
+        method:'PUT',
+        data: {
+          Name: Name, 
+          User:User,
+          LstName: LstName, 
+          Email: Email,
+          Rol: Rol,
+          Telefono: Telefono,
+          Estatus: Estatus
+        },
+        success: function (response) {
+          $('#getUsers').click();
+        }
+      })
+
+    });
+
+  $('table').on ('click', '.delete-button', function() {
+    let row = $(this).closest('tr');
+    let id = row.find('.id').text();
+
+    $.ajax({
+      url: '/usuarios/' + id,
+      method: 'DELETE',
+      success: function (response) {
+        console.log (response);
+        $('#getUsers').click();
+      }
     })
+
+  })
 })
